f85429fbb64e3dec40c6059a05bb884ef520ec9d
zuevandrey <bremlington@gmail.com>
2024-11-29 22:43:51 +0300

    added remove methods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,35 @@
 public class Main {
     public static void main(String[] args) {
-        CustomList myList = new CustomList();
+        CustomList myList = new CustomList(2);
+
         myList.add(10);
-        System.out.println(myList.size());
-
-        myList.printElements();
-
         myList.add(20);
-        System.out.println(myList.size());
+        myList.add(30);
+        myList.add(40);
+        myList.add(40);
 
         myList.printElements();
-
-        myList.add(0);
         System.out.println(myList.size());
 
+        myList.removeByIndex(0);
+        myList.removeByIndex(1);
         myList.printElements();
+        System.out.println(myList.size());
+
+        myList.add(60);
+        myList.printElements();
+        System.out.println(myList.size());
+
+        myList.removeElement(20);
+        myList.printElements();
+        System.out.println(myList.size());
+
+        myList.removeElement(40);
+        myList.printElements();
+        System.out.println(myList.size());
+
+        myList.removeElement(60);
+        myList.printElements();
+        System.out.println(myList.size());
     }
 }
