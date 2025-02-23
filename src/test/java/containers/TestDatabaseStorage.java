bb2b7faa14f60edf5f8a7467bc5745178a048bd4
zuevandrey <bremlington@gmail.com>
2025-02-23 13:01:17 +0300

    test reorganize

--- src/test/java/containers/TestDatabaseStorage.java
+++ src/test/java/containers/TestDatabaseStorage.java
@@ -2,20 +2,49 @@ package containers;
 
 import collections.CustomLinkedList;
 import collections.CustomList;
+import collections.List;
+import containers.database.StorageConfig;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestDatabaseStorage {
 
+    private final StorageType storageType = StorageType.DATABASE;
+
     @Test
-    void testDatabaseStorageNotLinked(){
+    void testDatabaseStorageList(){
         CustomList myList = new CustomList();
-        TestStorage.testDatabaseStorage(myList, false);
+        testDatabaseStorage(myList, false);
+
+        CustomLinkedList myListLinked = new CustomLinkedList();
+        testDatabaseStorage(myListLinked, true);
     }
 
-    @Test
-    void testDatabaseStorageLinked(){
-        CustomLinkedList myList = new CustomLinkedList();
-        TestStorage.testDatabaseStorage(myList, true);
-    }
+    void testDatabaseStorage(List myList, boolean isLinked){
+        myList.add(30);
+        myList.add(10);
+        myList.add(10);
+        myList.add(40);
 
+        StorageConfig storageConfig = new StorageConfig(storageType);
+        StorageUnit storageUnit = new StorageUnit(storageType, storageConfig);
+        StorageSaveResult saveResult = storageUnit.save(myList);
+        assertTrue(saveResult.success);
+        assertNotEquals(saveResult.id, "");
+
+        StorageLoadResult loadResult = storageUnit.load(saveResult.id, isLinked);
+        List newList = loadResult.list;
+        assertEquals(4, newList.getSize());
+        assertEquals(30, newList.getByIndex(0));
+        assertEquals(10, newList.getByIndex(1));
+        assertEquals(10, newList.getByIndex(2));
+        assertEquals(40, newList.getByIndex(3));
+
+        StorageConfig incorrectConfig = new StorageConfig("incorrect", "incorrect", "incorrect", 0, "incorrect", "incorrect");
+
+        StorageUnit storageIncorrectUnit = new StorageUnit(storageType, incorrectConfig);
+        StorageSaveResult saveIncorrectResult = storageIncorrectUnit.save(myList);
+        assertFalse(saveIncorrectResult.success);
+    }
 }
--- src/test/java/containers/TestFileStorage.java
+++ src/test/java/containers/TestFileStorage.java
@@ -2,19 +2,48 @@ package containers;
 
 import collections.CustomLinkedList;
 import collections.CustomList;
+import collections.List;
+import containers.database.StorageConfig;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TestFileStorage {
+    private final StorageType storageType = StorageType.FILE;
+
     @Test
-    void testFileStorageNotLinked(){
+    void testFileStorageList(){
         CustomList myList = new CustomList();
-        TestStorage.testFileStorage(myList, false);
+        testFileStorage(myList, false);
+
+        CustomLinkedList myListLinked = new CustomLinkedList();
+        testFileStorage(myListLinked, true);
     }
 
-    @Test
-    void testFileStorageLinked(){
-        CustomLinkedList myList = new CustomLinkedList();
-        TestStorage.testFileStorage(myList, true);
-    }
+    void testFileStorage(List myList, boolean isLinked){
+        myList.add(30);
+        myList.add(10);
+        myList.add(10);
+        myList.add(40);
 
+        StorageConfig storageConfig = new StorageConfig(storageType);
+        StorageUnit storageUnit = new StorageUnit(storageType, storageConfig);
+        StorageSaveResult saveResult = storageUnit.save(myList);
+        assertTrue(saveResult.success);
+        assertNotEquals(saveResult.id, "");
+
+        StorageLoadResult loadResult = storageUnit.load(saveResult.id, isLinked);
+        List newList = loadResult.list;
+        assertEquals(4, newList.getSize());
+        assertEquals(30, newList.getByIndex(0));
+        assertEquals(10, newList.getByIndex(1));
+        assertEquals(10, newList.getByIndex(2));
+        assertEquals(40, newList.getByIndex(3));
+
+        StorageConfig incorrectConfig = new StorageConfig("///////");
+
+        StorageUnit storageIncorrectUnit = new StorageUnit(storageType, incorrectConfig);
+        StorageSaveResult saveIncorrectResult = storageIncorrectUnit.save(myList);
+        assertFalse(saveIncorrectResult.success);
+    }
 }
